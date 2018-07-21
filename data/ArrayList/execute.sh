97519406667a652702f808812a881418cf9b22c5
duy <lebuitienduy@gmail.com>
2018-07-22 06:27:53 +0800

    update all execute.sh

--- data/ArrayList/execute.sh
+++ data/ArrayList/execute.sh
@@ -1,1 +1,3 @@
 python3 ../../DSM.py --data_dir input_traces --save_dir saved_model --work_dir work_dir
+#python3 ../../DSM.py --old_fsm work_dir/FINAL_mindfa.txt --additional_trace work_dir/rejected_traces/ --work_dir 2nd_workdir --save_dir saved_model/
+
